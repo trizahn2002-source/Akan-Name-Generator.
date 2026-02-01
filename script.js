35facf343edbbb92a38ccc24d9f12d90b9d34ff2
Trizah Njeri <trizahn2002@gmail.com>
2026-02-01 22:42:09 +0300

    Added const in js

---
+++ script.js
@@ -0,0 +1,5 @@
+const dayNames ={"Sunday" , "Monday" , "Tuesday", "Wednesday" , "Thursday" , "Friday" ,"Saturday"}
+const maleAkanNames = {"Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" , "Yaw"}
+const femaleAkanNames ={"Akosua" , "Adwoa" , "Abenaa" , "Akua" , "Yaa" , "Afua" , "Ama"}
+
+ 
